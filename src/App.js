f8679bc4d5d1e3da4d6c6717d6cdca689f224d95
Jonatan Lehtinen <jonatan.lehtinen@hotmail.com>
2018-01-10 18:15:28 +0200

    App.js: Add basic structure

--- src/App.js
+++ src/App.js
@@ -1,19 +1,101 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import {PageHeader} from 'react-bootstrap';
 import './App.css';
+
+
+function Sighting(props) {
+  return (
+    <div>
+      <h4>{props.time}</h4>
+      <h5>{props.species}</h5>
+      <h5>{props.description}</h5>
+      <h5>{props.count}</h5>
+    </div>
+  );
+}
+
+class DuckSightingsList extends Component {
+  render() {
+    const sightings = [];
+    this.props.sightings.forEach((sighting) => {
+       sightings.push(
+         <Sighting
+         time="10.1.2018" 
+         species = "duck"
+         description = "weird"
+         count = "100" />
+       );
+    });
+    return (
+      <ul>
+        {sightings}
+      </ul>
+    );
+  }
+}
+
+class DuckSightingsSortButton extends Component {
+  render() {
+    return (
+      <h3>Here's the sort button</h3>
+    );
+  }
+}
+
+class DuckSightingsHeader extends Component {
+  render() {
+    return (
+      <h2>Header for the sightings</h2>
+    );
+  }
+}
+
+class DuckSightings extends Component {
+  render() {
+    return (
+      <div>
+      <DuckSightingsHeader />
+      <DuckSightingsSortButton />
+      <DuckSightingsList sightings={["2", "sd"]}/>
+      </div>
+    );
+  }
+}
+
+class ReportSightingForm extends Component {
+  render() {
+    return (
+      <h1>The form will go here</h1>   
+    );
+  }
+}
+
+class MainHeader extends Component {
+  render() {
+    return (
+       <PageHeader>This is the main header</PageHeader>
+    );
+  }
+}
+
+
+class DuckSightingsTable extends Component {
+  render() {
+    return (
+      <div>
+        <MainHeader />
+        <ReportSightingForm />
+        <DuckSightings />
+      </div>
+    );
+  }
+}
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <DuckSightingsTable />
     );
   }
 }
