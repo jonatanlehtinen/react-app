6945f737f0d3e32a4ff599c6c4271008cf1a1040
Jonatan Lehtinen <jonatan.lehtinen@hotmail.com>
2018-01-18 17:18:46 +0200

    config.json: Add url configs

---
+++ src/config.js
@@ -0,0 +1,7 @@
+const apiUrl = "http://localhost:8081/";
+const apiSightingsPath = "sightings";
+const apiSpeciesPath = "species";
+
+export const config = { apiUrl: apiUrl, 
+                        apiSightingsPath: apiSightingsPath,
+                        apiSpeciesPath: apiSpeciesPath };
